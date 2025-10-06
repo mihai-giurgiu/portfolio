ba1cf4b4f1c319fbe88cc81f1f3f0fbf91430eb3
Mihai Giurgiu <mihaigiurgiu100@gmail.com>
2025-10-06 23:48:51 +0300

    Adding description to the projects

--- src/data/projectData.tsx
+++ src/data/projectData.tsx
@@ -15,7 +15,7 @@ export const projects: Project[] = [
     image: "../assets/BentikThumbnail2.png",
     subtitles: ["Website Design"],
     description:
-      "A modern and sleek website design for Bentik Music, showcasing their portfolio and services.",
+      "I collaborated with Bentik Music to design and develop an intuitive and responsive website that reflects the studio’s identity. The goal was to create a digital space that communicates trust and musical expertise while keeping navigation simple. The structure was built around clarity: a clean homepage introducing the studio, detailed service sections for recording, mixing, and mastering, and an integrated contact form for quick inquiries.The end result is a functional platform that helps Bentik Music showcase its work and connect effortlessly with new clients.",
     gallery: [
       "../assets/BentikWebsite1-min.jpg",
       "../assets/BentikWebsite2-min.jpg",
@@ -28,6 +28,7 @@ export const projects: Project[] = [
     title: "Art School Timisoara",
     image: "../assets/LogoAP_Thumbnail.jpg",
     subtitles: ["Logo Design", "Website Design"],
+    description: "For Arte Plastice High School, I developed both the brand identity and the website. The challenge was making a site that feels artistic but functions with the precision of an academic platform. On the web side, I built a responsive WordPress site, with dynamic content sections that allow staff to easily update news, exhibitions, and student catalogs. The color palette, typography, and layout were chosen to highlight student projects and express the school's creative side. The logo design focuses on symmetry and geometric balance reflecting the artistic principles taught at the school.",
     gallery: [
       "../assets/Website1-min.jpg",
       "../assets/LogoAP_Thumbnail.jpg",
@@ -53,7 +54,7 @@ export const projects: Project[] = [
     image: "../assets/Teren_Showcase3-min.jpg",
     subtitles: ["UI Design", "Development"],
     description:
-      "Teren was an application designed for football addicts to find a field where they could satisfy their cravings. With its simple and concise design, the app created better interaction between a player and a field. It also offered features such as notifications, calendar, and integration with other collaboration tools.",
+      "Teren was an app created for football enthusiasts looking to easily find and book nearby fields. With a clean and concise design, it simplified the connection between players and venues, offering features like notifications, calendar integration, and collaborative tools to make organizing matches easier.",
     gallery: [
       "../assets/login_page.jpg",
       "../assets/field_details_page.jpg",
@@ -64,6 +65,7 @@ export const projects: Project[] = [
     title: "Sofia Smile | Dental Clinic",
     image: "../assets/LogoSofiaSmile-03.png",
     subtitles: ["Logo Design", "Website Design"],
+    description: "Sofia Smile's website is built to reflect the professionalism of a modern dental clinic, making it easy for visitors to explore services, view transparent pricing, and book appointments in just a few clicks.",
     gallery: [
       "../assets/SofiaSmile_Website1-min.jpg",
       "../assets/SofiaSmile_Website2-min.jpg",
@@ -75,6 +77,7 @@ export const projects: Project[] = [
     title: "Satchinez Secondary School",
     image: "/assets/SatchinezThumbnail.jpg",
     subtitles: ["Website Design"],
+    description:"Scoala Satchinez was a really uplifthing project, building a website to give the school a friendly online presence. The goal was to create a space where parents, teachers, and students could easily find information about news, activities, and events.",
     gallery: [
       "../assets/SatchinezWebsite1-min.jpg",
       "../assets/SatchinezWebsite2-min.jpg",
@@ -87,7 +90,7 @@ export const projects: Project[] = [
     image: "../assets/Valtian_Logo1-min.jpg",
     subtitles: ["Logo Design"],
     description:
-      "Teren was an application designed for football addicts to find a field where they could satisfy their cravings. With its simple and concise design, the app created better interaction between a player and a field. It also offered features such as notifications, calendar, and integration with other collaboration tools.",
+      "I created a kind of logo that holds up well on signage, menus, and digital, reflecting the restaurant’s identity wherever people encounter it. The shape was composed to feel sturdy and grounded, yet approachable, symmetry and structure give it a confident silhouette, while slight curvatures soften the edges and make the design feel human rather than rigid.",
     gallery: [
       "../assets/Valtian_Logo1-min.jpg",
       "../assets/Valtian_Logo2-min.jpg",
@@ -111,6 +114,7 @@ export const projects: Project[] = [
     title: "Sauca & Partners | Lawyer",
     image: "../assets/Sauca_TrifoldBrochure2-min.jpg",
     subtitles: ["Brochure", "Print Design"],
+    description:"I designed this trifold brochure for Saucă & Partners, a law firm based in Timișoara, with the goal of presenting their services in a structured way. The design focuses on clarity and readability, guiding the reader naturally through the firm’s story, contact details, and key values.",
     gallery: [
       "../assets/Sauca_TrifoldBrochure2.jpg",
       "../assets/Sauca_TrifoldBrochure3-min.jpg",
